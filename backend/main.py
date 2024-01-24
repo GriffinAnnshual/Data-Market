4e78cca5f555e2cd90e3a39d6568e1b81bd83fec
Griffin Annshual <griffintbr@gmail.com>
2024-01-24 19:41:27 +0530

    feat(api): add Flask routes for email verification

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, redirect, url_for, session
 from passlib.hash import bcrypt
 from flask_cors import CORS
-from flask import session
 import jwt
 from passlib.hash import bcrypt
 import mysql.connector
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -89,7 +91,7 @@ def login():
             return jsonify({'success': False, 'message': 'Invalid password'}), 401
         
         token = jwt.encode(
-            {'email': email, 'name': user[1], 'password': user[2]},
+            {'email': email, 'name': user[0], 'password': user[2]},
             'secretKey',
             algorithm='HS256'
         )
@@ -98,6 +100,83 @@ def login():
     except Exception as e:
         return jsonify({'success': False,'message': str(e)}), 401
     
+
+@app.route("/send-otp", methods=["POST"])
+def send():
+    try:
+        data = request.json
+        email = data.get('email')
+        username = data.get('username')
+        connection = DBConnection()
+        cursor = connection.cursor()
+
+        if user_exists(cursor, 'username', username):
+            return jsonify({'exists': True, 'message': 'Username Already taken'}), 401
+        if user_exists(cursor, 'email', email):
+            return jsonify({'exists': True, 'message': 'Email Already taken'}), 401
+        session["email"] = email
+        sender_email = "noreply.gmarket@gmail.com"
+        receiver_email = email
+
+        token = jwt.encode(
+            {'email': email},
+            'secretKey',
+            algorithm='HS256'
+        )
+        html_content = """
+        <html>
+        <body>
+            <h3>Dear {0},</h3>
+            <br></br>
+            <p style="text-align:justify; color:black;">Welcome to the UserMarket, where you get to see lot of users!</p>
+            <p style="text-align:justify; color:black;">Verify our Email by clicking on the below button:</b></p>
+            <a href="http://127.0.0.1:5000/verify/email?verify={1}&email={2}">
+            <button style="padding:10px;border-radius:10%;background-color:skyblue;white-space:nowrap;color:black;font-weight:bold;">
+            <b>Verify now</b>
+            </button>
+            </a>
+        </body>
+        </html>
+        """.format(username, token, email)
+                
+        html_part = MIMEText(html_content, 'html')
+        message = MIMEMultipart()
+        message.attach(html_part)
+
+        message['From'] = "noreply.gmarket@gmail.com"
+        message['To'] = email
+        message['Subject'] = "Please Verify you Email - UserMarket"
+
+        smtp_server = "smtp.gmail.com"
+        smtp_port = 587
+        smtp_username = "noreply.gmarket@gmail.com"
+        smtp_password = "esgx wvli yrxl xkpv"
+
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(smtp_username, smtp_password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+
+        return jsonify({"success": True, "message": "Email sent successfully!"}), 200
+
+    except Exception as e:
+        return jsonify({"success": False, "message": str(e)}), 401
+
+
+@app.route("/verify/email", methods=["GET"])
+def verify():
+    try:
+        token = request.args.get('verify')
+        email = request.args.get('email')
+        data = jwt.decode(token, "secretKey", algorithms=['HS256'])
+        if data.get("email") == email:
+            response = make_response(redirect("http://localhost:5173/register"))
+            response.set_cookie('email_verified', 'true')
+            return response
+        else:
+            return jsonify({"success": False, "message": "Email not verified!"}), 401
+    except Exception as e:
+        return jsonify({"success": False, "message": str(e)}), 401
 
 @app.route("/home", methods=['GET'])
 def home():
